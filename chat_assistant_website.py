0a581b0c0b46d66b4d5c7e55b638e161b92db888
Dipit Jaywant <56427184+Jaydip1989@users.noreply.github.com>
2023-06-28 17:30:29 +0530

    Add files via upload

---
+++ chat_assistant_website.py
@@ -0,0 +1,40 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages = [
+    {
+        'role':'system',
+        'content':'You are a dietitian'
+    }
+]
+
+def CustomChatGPT(user_input):
+    messages.append(
+        {
+            "role":'user',
+            'content':user_input
+        }
+    )
+    response=openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response['choices'][0]['message']['content']
+    messages.append(
+        {
+            "role":'assistant',
+            'content':ChatGPT_reply
+        }
+    )
+    return ChatGPT_reply
+
+demo  = gradio.Interface(
+    fn = CustomChatGPT,
+    inputs="text",
+    outputs = "text",
+    title="Your Title"
+)
+
+demo.launch(share=True)
